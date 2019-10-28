00b5d341009c9bd5a5b57fa123e73165bff1e869
Ihor Myroniuk <ihormyroniuk@gmail.com>
2019-10-28 17:29:27 +0200

    Initial Commit

---
+++ ACoreAnimation/ACoreAnimation.h
@@ -0,0 +1,19 @@
+//
+//  ACoreAnimation.h
+//  ACoreAnimation
+//
+//  Created by Ihor Myroniuk on 10/28/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ACoreAnimation.
+FOUNDATION_EXPORT double ACoreAnimationVersionNumber;
+
+//! Project version string for ACoreAnimation.
+FOUNDATION_EXPORT const unsigned char ACoreAnimationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACoreAnimation/PublicHeader.h>
+
+
