03a3c42d5bc9627e335bea27bee9ad294f895e16
Ihor Myroniuk <ihormyroniuk@gmail.com>
2019-10-28 17:53:50 +0200

    dfsf

--- ACoreAnimation/ACATextLayer.swift
+++ ACoreAnimation/ACATextLayer.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2019 Ihor Myroniuk. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public class ACATextLayer : CATextLayer {
+    
+    public override func draw(in context: CGContext) {
+        let height = self.bounds.size.height
+        let fontSize = self.fontSize
+        let yDiff = (height - fontSize) / 2 - fontSize / 10
+        
+        context.saveGState()
+        context.translateBy(x: 0, y: yDiff) // Use -yDiff when in non-flipped coordinates (like macOS's default)
+        super.draw(in: context)
+        context.restoreGState()
+    }
+    
+}
