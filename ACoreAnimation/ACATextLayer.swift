ab14021dac94eee809d5b8dde4ce25a27366da5e
Ihor Myroniuk <ihormyroniuk@gmail.com>
2019-11-05 12:16:30 +0200

    no message

--- ACoreAnimation/ACATextLayer.swift
+++ ACoreAnimation/ACATextLayer.swift
@@ -8,7 +8,45 @@
 
 import UIKit
 
-public class ACATextLayer : CATextLayer {
+open class ACATextLayer: CATextLayer {
+    
+    // MARK: Initializers
+    
+    public override init() {
+        super.init()
+        setup()
+    }
+    
+    public override init(layer: Any) {
+        super.init(layer: layer)
+        setup()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    open func setup() {
+        
+    }
+    
+    // MARK: Layout
+    
+    open override func layoutSublayers() {
+        super.layoutSublayers()
+        layout()
+    }
+    
+    open func layout() {
+        
+    }
+    
+}
+
+public class ACATextLayer2: CATextLayer {
     
     public override func draw(in context: CGContext) {
         let height = self.bounds.size.height
