0065850a47cb531922f070d1e56b5796ced45945
Ihor Myroniuk <ihormyroniuk@gmail.com>
2019-11-15 11:22:04 +0200

    no message

--- ACoreAnimation/ACATextLayer.swift
+++ ACoreAnimation/ACATextLayer.swift
@@ -46,13 +46,13 @@ open class ACATextLayer: CATextLayer {
     
 }
 
-public class ACATextLayer2: CATextLayer {
+public class ACATextLayerVerticalTextAlignmentCenter: CATextLayer {
     
     public override func draw(in context: CGContext) {
         let height = self.bounds.size.height
         let fontSize = self.fontSize
         let yDiff = (height - fontSize) / 2 - fontSize / 10
-        
+
         context.saveGState()
         context.translateBy(x: 0, y: yDiff) // Use -yDiff when in non-flipped coordinates (like macOS's default)
         super.draw(in: context)
