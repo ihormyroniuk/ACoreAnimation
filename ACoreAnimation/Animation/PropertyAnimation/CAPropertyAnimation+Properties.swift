c905f696d9d31267b2d7e66e8cf2a16e33be4f8e
Ihor Myroniuk <ihormyroniuk@gmail.com>
2023-05-08 14:13:49 +0300

    no message

--- ACoreAnimation/Animation/PropertyAnimation/CAPropertyAnimation+Properties.swift
+++ ACoreAnimation/Animation/PropertyAnimation/CAPropertyAnimation+Properties.swift
@@ -2,6 +2,7 @@ import QuartzCore
 
 /*
  https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/CoreAnimation_guide/AnimatableProperties/AnimatableProperties.html#//apple_ref/doc/uid/TP40004514-CH11-SW2
+ https://stackoverflow.com/questions/44230796/what-is-the-full-keypath-list-for-cabasicanimation
 */
 public extension CAPropertyAnimation {
     static let backgroundColor = "backgroundColor"
