e51eb9110d73a338d98ea86f4fd80b8edeca1c90
Ihor Myroniuk <ihormyroniuk@gmail.com>
2023-09-11 23:47:22 +0300

    no message

--- ACoreAnimation/Layer/CALayer+Animations.swift
+++ ACoreAnimation/Layer/CALayer+Animations.swift
@@ -8,60 +8,42 @@ public extension CALayer {
     @discardableResult
     func addAnimation(_ animation: CAAnimation) -> String {
         let key = UUID().uuidString
-        self.add(animation, forKey: key)
+        let animationCopy = animation.copy() as! CAAnimation
+        self.add(animationCopy, forKey: key)
         return key
     }
     
-    private var didStartAnimationHandlers: NSMutableDictionary {
-        get {
-            guard let nsDictionary = objc_getAssociatedObject(self, &DidStartAnimationHandlersAssotiationKey) as? NSMutableDictionary else {
-                let nsDictionary = NSMutableDictionary()
-                objc_setAssociatedObject(self, &DidStartAnimationHandlersAssotiationKey, nsDictionary, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-                return self.didStartAnimationHandlers
-            }
-            return nsDictionary
-        }
-    }
+    private var didStartAnimationHandlerKey: String { "\(Unmanaged.passUnretained(self).toOpaque())-didStartAnimationHandlerKey" }
+    private var didStopAnimationHandlerKey: String { "\(Unmanaged.passUnretained(self).toOpaque())-didStopAnimationHandlerKey" }
     
     @discardableResult
     func addAnimation(_ animation: CAAnimation, didStartAnimationHandler: @escaping () -> Void) -> String {
         let key = UUID().uuidString
-        animation.delegate = self
-        animation.setValue(key, forKey: "animationKey")
-        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: key as NSString)
-        self.add(animation, forKey: key)
+        let animationCopy = animation.copy() as! CAAnimation
+        animationCopy.delegate = self
+        animationCopy.setValue(didStartAnimationHandler, forKey: didStartAnimationHandlerKey)
+        self.add(animationCopy, forKey: key)
         return key
-    }
-
-    private var didStopAnimationHandlers: NSMutableDictionary {
-        get {
-            guard let nsDictionary = objc_getAssociatedObject(self, &DidStopAnimationHandlersAssotiationKey) as? NSMutableDictionary else {
-                let nsDictionary = NSMutableDictionary()
-                objc_setAssociatedObject(self, &DidStopAnimationHandlersAssotiationKey, nsDictionary, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
-                return self.didStopAnimationHandlers
-            }
-            return nsDictionary
-        }
     }
 
     @discardableResult
     func addAnimation(_ animation: CAAnimation, didStopAnimationHandler: @escaping (Bool) -> Void) -> String {
         let key = UUID().uuidString
-        animation.delegate = self
-        animation.setValue(key, forKey: "animationKey")
-        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: key as NSString)
-        self.add(animation, forKey: key)
+        let animationCopy = animation.copy() as! CAAnimation
+        animationCopy.delegate = self
+        animationCopy.setValue(didStopAnimationHandler, forKey: didStopAnimationHandlerKey)
+        self.add(animationCopy, forKey: key)
         return key
     }
     
     @discardableResult
     func addAnimation(_ animation: CAAnimation, didStartAnimationHandler: @escaping () -> Void, didStopAnimationHandler: @escaping (Bool) -> Void) -> String {
         let key = UUID().uuidString
-        animation.delegate = self
-        animation.setValue(key, forKey: "animationKey")
-        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: key as NSString)
-        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: key as NSString)
-        self.add(animation, forKey: key)
+        let animationCopy = animation.copy() as! CAAnimation
+        animationCopy.delegate = self
+        animationCopy.setValue(didStartAnimationHandler, forKey: didStartAnimationHandlerKey)
+        animationCopy.setValue(didStopAnimationHandler, forKey: didStopAnimationHandlerKey)
+        self.add(animationCopy, forKey: key)
         return key
     }
 
@@ -70,17 +52,34 @@ public extension CALayer {
 extension CALayer: CAAnimationDelegate {
 
     public func animationDidStart(_ anim: CAAnimation) {
-        guard let key = anim.value(forKey: "animationKey") as? String else { return }
-        guard let didStartAnimationHandler = didStartAnimationHandlers.object(forKey: key as NSString) as? (() -> Void) else { return }
-        didStartAnimationHandlers.removeObject(forKey: key as NSString)
+        guard let didStartAnimationHandler = anim.value(forKey: didStartAnimationHandlerKey) as? (() -> Void) else { return }
         didStartAnimationHandler()
     }
 
     public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
         guard let key = anim.value(forKey: "animationKey") as? String else { return }
-        guard let didStopAnimationHandler = didStopAnimationHandlers.object(forKey: key as NSString) as? ((Bool) -> Void) else { return }
-        didStopAnimationHandlers.removeObject(forKey: key as NSString)
+        guard let didStopAnimationHandler = anim.value(forKey: didStopAnimationHandlerKey) as? ((Bool) -> Void) else { return }
         didStopAnimationHandler(flag)
     }
 
+}
+
+private class DidStartAnimationHandlerHolder: NSObject {
+    
+    let didStartAnimationHandler: () -> Void
+    
+    init(didStartAnimationHandler: @escaping () -> Void) {
+        self.didStartAnimationHandler = didStartAnimationHandler
+    }
+    
+}
+
+class DidStopAnimationHandlerHolder: NSObject {
+    
+    let didStopAnimationHandler: (Bool) -> Void
+    
+    init(didStopAnimationHandler: @escaping (Bool) -> Void) {
+        self.didStopAnimationHandler = didStopAnimationHandler
+    }
+    
 }
