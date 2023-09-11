68fcf8b47a81e8947f43c9d6c74cf622f76153a2
Ihor Myroniuk <ihormyroniuk@gmail.com>
2023-09-11 23:12:07 +0300

    Revert "no message"
    This reverts commit b825da6371df41558e368378ae9351ebbfad25a9.

--- ACoreAnimation/Layer/CALayer+Animations.swift
+++ ACoreAnimation/Layer/CALayer+Animations.swift
@@ -27,7 +27,7 @@ public extension CALayer {
     func addAnimation(_ animation: CAAnimation, didStartAnimationHandler: @escaping () -> Void) -> String {
         let key = UUID().uuidString
         animation.delegate = self
-        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: animation)
+        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: key as NSString)
         self.add(animation, forKey: key)
         return key
     }
@@ -47,7 +47,7 @@ public extension CALayer {
     func addAnimation(_ animation: CAAnimation, didStopAnimationHandler: @escaping (Bool) -> Void) -> String {
         let key = UUID().uuidString
         animation.delegate = self
-        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: animation)
+        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: key as NSString)
         self.add(animation, forKey: key)
         return key
     }
@@ -56,8 +56,8 @@ public extension CALayer {
     func addAnimation(_ animation: CAAnimation, didStartAnimationHandler: @escaping () -> Void, didStopAnimationHandler: @escaping (Bool) -> Void) -> String {
         let key = UUID().uuidString
         animation.delegate = self
-        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: animation)
-        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: animation)
+        didStartAnimationHandlers.setObject(didStartAnimationHandler, forKey: key as NSString)
+        didStopAnimationHandlers.setObject(didStopAnimationHandler, forKey: key as NSString)
         self.add(animation, forKey: key)
         return key
     }
@@ -67,14 +67,16 @@ public extension CALayer {
 extension CALayer: CAAnimationDelegate {
 
     public func animationDidStart(_ anim: CAAnimation) {
-        guard let didStartAnimationHandler = didStartAnimationHandlers.object(forKey: anim) as? (() -> Void) else { return }
-        didStartAnimationHandlers.removeObject(forKey: anim)
+        guard let key = animationKeys()?.first(where: { anim == animation(forKey: $0) }) else { return }
+        guard let didStartAnimationHandler = didStartAnimationHandlers.object(forKey: key as NSString) as? (() -> Void) else { return }
+        didStartAnimationHandlers.removeObject(forKey: key as NSString)
         didStartAnimationHandler()
     }
 
     public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        guard let didStopAnimationHandler = didStopAnimationHandlers.object(forKey: anim) as? ((Bool) -> Void) else { return }
-        didStopAnimationHandlers.removeObject(forKey: anim)
+        guard let key = animationKeys()?.first(where: { anim == animation(forKey: $0) }) else { return }
+        guard let didStopAnimationHandler = didStopAnimationHandlers.object(forKey: key as NSString) as? ((Bool) -> Void) else { return }
+        didStopAnimationHandlers.removeObject(forKey: key as NSString)
         didStopAnimationHandler(flag)
     }
 
